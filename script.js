4c298df4939746722edd66f6a32ff0124612c173
Amol shelke <shelkeamol897@gmail.com>
2022-05-19 19:58:47 +0530

    Added More Memes

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const memesSrc = [
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_-BveXtt73oa2x0hUtj5h2yK7xEB3gAxwGpF-lwgmTo6cddUDtI8RZT7PuvlGX6hRZYM&usqp=CAU",
   "https://1.bp.blogspot.com/-UlFIpSZcNS0/YJIgP1BEzmI/AAAAAAAAETE/zWEtrl3PqmgjSwJx9ccIlqM3yW_toEZOgCLcBGAsYHQ/s2048/very-funny-indian-memes.jpg",
   "https://1.bp.blogspot.com/-gwaDLFVz8Uc/YNtP0yBouaI/AAAAAAAAB5w/10SEpkv0LIAlvUAv_kCKN58u6wa_auURQCNcBGAsYHQ/s1079/Indian%2Bmemes%2Bin%2Bhindi%2Btrending%2BIndian%2Bmemes18-min.webp",
+  "https://images.news18.com/ibnlive/uploads/2021/09/cheese-butter.png",
+  "https://images.news18.com/ibnlive/uploads/2014/07/engineeringmemes1.jpg?impolicy=website&width=510&height=356",
+  "https://www.ohyaaro.com/wp-content/uploads/2021/05/funny-memes00.jpg",
+  "https://memezila.com/wp-content/When-its-raining-but-the-electricity-doesnt-go-Miracle-miracle-meme-2239.png",
 ];
 
 const memeImg = document.querySelector(".meme");
